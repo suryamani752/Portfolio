13940db77046f42f9aaa0ea4c9f30fbeef710668
Suryamani kumar <124435021+suryamani752@users.noreply.github.com>
2024-09-29 18:57:18 +0530

    add new project

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -2,7 +2,6 @@
 import dice from "../../assets/Dice/Dice.png";
 // import expand from "../../assets/expand/expand1.png";
 import libray from "../../assets/library/library.png";
-import recipe from "../../assets/recipe/recipe1.png";
 import expand1 from "../../assets/expand/expand1.png";
 import expand2 from "../../assets/expand/expand2.png";
 import brand1 from "../../assets/brand/brand1.png";
@@ -11,14 +10,32 @@ import crypto2 from "../../assets/cryptcurrency/2.png";
 import crypto3 from "../../assets/cryptcurrency/3.png";
 import crypto4 from "../../assets/cryptcurrency/4.png";
 import crypto5 from "../../assets/cryptcurrency/5.png";
+import foodMenu1 from "../../assets/Food-Menu/f1.png"
+import foodMenu2 from "../../assets/Food-Menu/f2.png"
+import foodMenu3 from "../../assets/Food-Menu/f3.png"
+import foodMenu4 from "../../assets/Food-Menu/f4.png"
+import foodMenu5 from "../../assets/Food-Menu/f5.png"
+import foodMenu6 from "../../assets/Food-Menu/f6.png"
+import foodMenu7 from "../../assets/Food-Menu/f7.png"
+import foodMenu8 from "../../assets/Food-Menu/f8.png"
 
 export const projectsData = [
+  {
+    title: "Food-Menu",
+    link: "https://portfolio-suryamani-kumar-752.vercel.app/",
+    repo: "https://github.com/suryamani752/food-menu-Suryamani-frontend-Developer",
+    live: "Live Demo open in Laptop or PC because maybe some projects are not responsive.",
+    subtitle: "React.js | tailwind CSS | Axios",
+    category: "coding",
+    description: "",
+    images: [foodMenu1, foodMenu2, foodMenu3, foodMenu4, foodMenu5, foodMenu6, foodMenu7, foodMenu8],
+  },
   {
     title: "CryptoCurrency",
     link: "https://cryptocurrency-suryamani752.vercel.app/",
     repo: "https://github.com/suryamani752/cryptocurrency",
     live: "Live Demo open in Laptop or PC because maybe some projects are not responsive.",
-    subtitle: "React.js | tailwind CSS | Axios",
+    subtitle: "React.js | tailwind CSS | Axios | react-spinners",
     category: "coding",
     description: "",
     images: [crypto1, crypto2, crypto3, crypto4, crypto5],
@@ -32,17 +49,6 @@ export const projectsData = [
     category: "coding",
     description: "",
     images: [dice],
-  },
-
-  {
-    title: "Recipe Finder",
-    link: "https://recipe-finder-client-wheat.vercel.app/",
-    repo: "https://github.com/suryamani752/recipe-finder-client",
-    live: "Live Demo open in Laptop or PC because maybe some projects are not responsive.",
-    subtitle: "React.js | bootstrap | sweetalert | react-cookie | axios",
-    category: "coding",
-    description: "",
-    images: [recipe],
   },
   {
     title: "Full-stack Library-Management app",
